16953d810eeab6f522829162d75df1dc4a066e0c
abhishek biranje <programwithabhi@gmail.com>
2026-03-16 16:14:14 +0530

    delection file format in nvim

--- array/deletion.c
+++ array/deletion.c
@@ -1,30 +1,30 @@
 
 #include <stdio.h>
 
-void display(int arr[], int arrSize){
-   for (int i = 0; i < arrSize; i++) {
+void display(int arr[], int arrSize) {
+  for (int i = 0; i < arrSize; i++) {
     printf("%d ", arr[i]);
-   } 
+  }
 }
 
-void deletion(int arr[], int arrSize, int indexToDelete){
-   for (int i = indexToDelete; i < arrSize - 1; i++) {
+void deletion(int arr[], int arrSize, int indexToDelete) {
+  for (int i = indexToDelete; i < arrSize - 1; i++) {
     arr[i] = arr[i + 1];
-   }
+  }
 }
 
 int main() {
-   int arr[100] = {7, 8, 22, 88};
-   int arrSize = 4;
+  int arr[100] = {7, 8, 22, 88};
+  int arrSize = 4;
 
-   printf("Before Deletion: ");
-   display(arr, arrSize);
-   printf("\n");
+  printf("Before Deletion: ");
+  display(arr, arrSize);
+  printf("\n");
 
-   printf("After Deletion: ");
-   deletion(arr, arrSize, 2);
-   arrSize -= 1;
-   display(arr, arrSize);
+  printf("After Deletion: ");
+  deletion(arr, arrSize, 2);
+  arrSize -= 1;
+  display(arr, arrSize);
 
-   return 0; 
+  return 0;
 }
