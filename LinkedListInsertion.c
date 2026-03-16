2218b568b8b4b8b60cdce59260226886d61f5a45
Abhishek <programwithabhi@gmail.com>
2026-03-16 15:32:32 +0530

    Linked list insertion methods: insert at beginning and insert in end

---
+++ LinkedListInsertion.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+  int data;
+  struct Node *next;
+};
+
+void DisplayLinkedList(struct Node *ptr) {
+  while (ptr != NULL) {
+    printf("Element: %d\n", ptr->data);
+    ptr = ptr->next;
+  }
+}
+
+struct Node *insertAtBeginningOfLinkedList(struct Node *head, int data) {
+  struct Node *ptr = (struct Node *)malloc(sizeof(struct Node));
+  ptr->data = data;
+  ptr->next = head;
+  return ptr;
+}
+
+void insertAtEndOfLinkedList(struct Node *head, int data) {
+  struct Node *ptr = (struct Node *)malloc(sizeof(struct Node));
+  ptr->data = data;
+  struct Node *p = head;
+
+  while (p->next != NULL) {
+    p = p->next;
+  }
+  p->next = ptr;
+  ptr->next = NULL;
+}
+
+void insertAtIndexOfLinkedList(struct Node *head, int data, int index) {
+  struct Node *ptr = (struct Node *)malloc(sizeof(struct Node));
+  ptr->data = data;
+  struct Node *p = head;
+
+  while (p->next != NULL) {
+    p = p->next;
+  }
+  p->next = ptr;
+  ptr->next = NULL;
+}
+
+void main() {
+  struct Node *head, *second, *third;
+
+  head = (struct Node *)malloc(sizeof(struct Node));
+  second = (struct Node *)malloc(sizeof(struct Node));
+  third = (struct Node *)malloc(sizeof(struct Node));
+
+  head->data = 7;
+  head->next = second;
+
+  second->data = 8;
+  second->next = third;
+
+  third->data = 9;
+  third->next = NULL;
+
+  printf("LinkedList Before Insertion\n");
+  DisplayLinkedList(head);
+
+  printf("\nLinkedList After Insertion\n");
+  // head = insertAtBeginningOfLinkedList(head, 6);
+  insertAtEndOfLinkedList(head, 10);
+  DisplayLinkedList(head);
+}
