e877c81b25805a9a2162fe0e40c8e9a1f22b93bb
Abhishek <programwithabhi@gmail.com>
2026-03-18 20:27:36 +0530

    Created a proper folder structure to manage code

---
+++ linked-list/deletion/deletion.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Linked List initialization
+struct Node {
+  int data;
+  struct Node *next;
+};
+
+// Display Linked List Fn
+void DisplayLinkedList(struct Node *ptr) {
+  while (ptr != NULL) {
+    printf("Element: %d\n", ptr->data);
+    ptr = ptr->next;
+  }
+}
+
+// Case first: Delete the first node from linked list
+
+void main() {
+  struct Node *head, *second, *third;
+
+  head = (struct Node *)malloc(sizeof(struct Node));
+  second = (struct Node *)malloc(sizeof(struct Node));
+  third = (struct Node *)malloc(sizeof(struct Node));
+
+  head->data = 7;
+  head->next = second;
+
+  second->data = 8;
+  second->next = third;
+
+  third->data = 9;
+  third->next = NULL;
+
+  printf("LinkedList Before Deletion\n");
+  DisplayLinkedList(head);
+}
