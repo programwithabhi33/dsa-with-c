4efd9f42561a585f14383b0e8e5889d59e1f0c91
abhishek biranje <programwithabhi@gmail.com>
2026-03-20 13:20:58 +0530

    Insert at first method in circular linked list

--- linked-list/circular/traversal.c
+++ linked-list/circular/traversal.c
@@ -17,6 +17,20 @@ void DisplayLinkedList(struct Node *head) {
   } while (ptr != head);
 }
 
+struct Node *insertAtFirst(struct Node **head, int data) {
+  struct Node *ptr = (struct Node *)malloc(sizeof(struct Node));
+  struct Node *iterationPtr = (*head);
+
+  ptr->data = data;
+  ptr->next = (*head);
+
+  while (iterationPtr->next != (*head)) {
+    iterationPtr = iterationPtr->next;
+  }
+  iterationPtr->next = ptr;
+  return ptr;
+}
+
 void main() {
   struct Node *head, *second, *third;
 
@@ -33,5 +47,10 @@ void main() {
   third->data = 9;
   third->next = head;
 
+  printf("Linked List Before Insertion\n");
+  DisplayLinkedList(head);
+
+  printf("\nLinked List After Insertion\n");
+  head = insertAtFirst(&head, 6);
   DisplayLinkedList(head);
 }
